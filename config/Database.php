ed7faf3cfb8af192656b247f243b7712f876cfba
charleeo <charleeotaru@gmail.com>
2020-07-02 07:46:38 +0100

    payment first commit

--- config/Database.php
+++ config/Database.php
@@ -3,7 +3,7 @@
 class Database{
   public $conn;
   public function __construct(){
-    // $this->conn = new mysqli("localhost", "root", "linux", "moodle");
+    $this->conn = new mysqli("localhost", "root", "linux", "moodle");
     // $this->conn = new mysqli("localhost", "root", "", "ispaceshopping");
     
     if($this->conn->connect_error)
